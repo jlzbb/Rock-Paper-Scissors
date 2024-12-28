07b89eefc076573dfc812515cc9bd0714a337345
Leonjlz <101525079+jlzbb@users.noreply.github.com>
2024-12-28 14:17:43 -0500

    create function for a single round gameplay

--- javascript.js
+++ javascript.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
         return "scissors";
 }
 
-let humanChoice = prompt("enter paper, rock, or scissors!");
+let humanChoice = (prompt("enter paper, rock, or scissors!")).toLowerCase();
 
 function getHumanChoice() {
     if (humanChoice == "paper")
@@ -26,3 +26,34 @@ function getHumanChoice() {
     else (humanChoice == 0 || undefined)
         return "try again!";
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanSelection == "rock" && computerSelection == "rock")
+        return "it's a tie, play again!";
+    else if (humanSelection == "rock" && computerSelection == "paper")
+        return `You lose! paper beats rock! Your score is ${humanScore} vs ${computerScore++}`;
+    else if (humanSelection == "rock" && computerSelection == "scissors")
+        return `You win! rock beats scissors! Your score is ${humanScore++} vs ${computerScore}`;
+    else if (humanSelection == "paper" && computerSelection == "paper")
+        return "it's a tie, play again!";
+    else if (humanSelection == "paper" && computerSelection == "rock")
+        return `You win! paper beats rock! Your score is ${humanScore++} vs ${computerScore}`;
+    else if (humanSelection == "paper" && computerSelection == "scissors")
+        return `You lose! scissors beat paper! Your score is ${humanScore} vs ${computerScore++}`;
+    else if (humanSelection == "scissors" && computerSelection == "paper")
+        return `You win! scissors beat paper! Your score is ${humanScore++} vs ${computerScore}`;
+    else if (humanSelection == "scissors" && computerSelection == "rock")
+        return `You lose! rock beats scissors! Your score is ${humanScore} vs ${computerScore++}`;
+    else if (humanSelection == "scissors" && computerSelection == "scissors")
+        return "it's a tie, play again!";
+    else (humanSelection == "try again!")
+        return "I told you to enter either paper rock or scissor!";
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
